eae7870e21986dd7b08e8c644c6cda928483722a
Jonathan Respeto <jrespeto@akamai.com>
2023-09-18 15:42:58 -0400

    changing dedicated to standard

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -29,7 +29,7 @@ resource "linode_instance" "master-1" {
     label = "k8s-master1"
     image = "linode/almalinux9"
     region = "us-southeast"
-    type = "g6-dedicated-4"
+    type = "g6-standard-4"
     authorized_keys = [linode_sshkey.k8s.ssh_key]
     root_pass = var.pass
     tags = [ "k8s", "labs" ]
@@ -39,7 +39,7 @@ resource "linode_instance" "node-11" {
     label = "k8s-node11"
     image = "linode/almalinux9"
     region = "us-southeast"
-    type = "g6-dedicated-4"
+    type = "g6-standard-4"
     authorized_keys = [linode_sshkey.k8s.ssh_key]
     root_pass = var.pass
     tags = [ "k8s", "labs" ]
@@ -49,7 +49,7 @@ resource "linode_instance" "node-12" {
     label = "k8s-node12"
     image = "linode/almalinux9"
     region = "us-southeast"
-    type = "g6-dedicated-4"
+    type = "g6-standard-4"
     root_pass = var.pass
     tags = [ "k8s", "labs" ]
 }
@@ -58,7 +58,7 @@ resource "linode_instance" "master-2" {
     label = "k8s-master2"
     image = "linode/almalinux9"
     region = "us-central"
-    type = "g6-dedicated-4"
+    type = "g6-standard-4"
     authorized_keys = [linode_sshkey.k8s.ssh_key]
     root_pass = var.pass
     tags = [ "k8s", "labs" ]
@@ -68,7 +68,7 @@ resource "linode_instance" "node-21" {
     label = "k8s-node21"
     image = "linode/almalinux9"
     region = "us-central"
-    type = "g6-dedicated-4"
+    type = "g6-standard-4"
     authorized_keys = [linode_sshkey.k8s.ssh_key]
     root_pass = var.pass
     tags = [ "k8s", "labs" ]
@@ -78,7 +78,7 @@ resource "linode_instance" "node-22" {
     label = "k8s-node22"
     image = "linode/almalinux9"
     region = "us-central"
-    type = "g6-dedicated-4"
+    type = "g6-standard-4"
     authorized_keys = [linode_sshkey.k8s.ssh_key]
     root_pass = var.pass
     tags = [ "k8s", "labs" ]
